050d001388984b5c8f2794579bfd0f5c28399e41
Pa-ppy <rayjacks07@gmail.com>
2025-04-24 17:28:00 +0000

    Complex types - string and int/float to tuple

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
-"""Module that provides a function to return a tuple from a string and a number."""
+"""Module that provides a function to return a tuple from a
+string and a number."""
 
 from typing import Union, Tuple
 
+
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-    """Return a tuple where the first element is a string and the second is the square of the number."""
+    """Return a tuple where the first element is a string and the
+    second is the square of the number."""
     return (k, float(v ** 2))
